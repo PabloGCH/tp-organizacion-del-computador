fc42a3327d96f20dc71f6cc4990b33abcb5c2969
mcumskov <maxicumskov@gmail.com>
2024-11-07 22:48:18 -0300

    Actualizo la UI para mostrar la información de los oficiales en una tabla

--- include/printStatistics.asm
+++ include/printStatistics.asm
@@ -8,17 +8,15 @@ section .data
     mBoxFormat db "| %c | %c | %c |", 10, 0
     mBoxOffset dq 0
     mCounter db 3
-    stTitle db "Estadísticas de los oficiales:", 10, 10, 0
-    saltoLinea db 10, 0
-    mOfficerTitle db "Oficial %hhi:", 10, 0
-    officerIndex db 1
-    mOfficerMovementFormat db "Dirección %hhi: %hhi movimientos", 10, 0
-    ; movs db 0,1,2,3,4,5,6,7
-    ; totalMovs equ $-movs
-    ; offset dq 0
-    ; arrow db "Flecha: %hhi", 10, 0
+    stTitle db "Estadísticas de los oficiales:", 10, 0
+    mBoxOfficersLine db "+-----------+-----------+-----------+", 10, 0
+    mBoxOfficersTitle db "|           | Oficial 1 | Oficial 2 |", 10, 0
+    mBoxOfficersFormat db "| DIR - %-3d | %-9hhi | %-9hhi |", 10, 0
+    mBoxOfficersKillsFormat db "| CAPTURAS  | %-9hhi | %-9hhi |", 10, 0
+
     officerOne db 5,2,5,6,72,1,2,2,8
     officerTwo db 0,1,2,3,4,5,6,7,8
+    saltoLinea db 10, 0
 section .bss
     ;officerOne resb 9
     ;officerTwo resb 9
@@ -28,19 +26,10 @@ main:
     ;mov [officerOne], rdi
     ;mov [officerTwo], rsi
 
-    print stMovements
     sub rsp, 8
     call printMovementsBox
     add rsp, 8
 
-    print stTitle
-
-    mov rdi, [officerOne]
-    sub rsp, 8
-    call printOfficerStats
-    add rsp, 8
-
-    mov rdi, [officerTwo]
     sub rsp, 8
     call printOfficerStats
     add rsp, 8
@@ -48,6 +37,9 @@ main:
     ret
 
 printMovementsBox:
+    print saltoLinea
+    print stMovements
+printMovementsBoxLoop:
     mov bl, [mCounter]
     cmp bl, 0
     jle finBoxLoop
@@ -68,32 +60,29 @@ printMovementsBox:
     add rsp, 8
 
     dec byte [mCounter]
-    jmp printMovementsBox
+    jmp printMovementsBoxLoop
 finBoxLoop:
     print mBoxLine
-    print saltoLinea
     ret
 
 printOfficerStats:
-    mov [officerAux], rdi
-    mov rsi, 0
-
-    mov rdi, mOfficerTitle
-    mov rsi, [officerIndex]
-    sub rsp, 8
-    call printf
-    add rsp, 8
+    print saltoLinea
+    print stTitle
+    print saltoLinea
+    print mBoxOfficersLine
+    print mBoxOfficersTitle
+    print mBoxOfficersLine
     mov byte [mCounter], 0
 loopOfficerMovements:
-
     mov rbx, 0
     mov bl, [mCounter]
     cmp bl, 8
     jge finOfficerMovementsLoop
 
-    mov rdi, mOfficerMovementFormat
+    mov rdi, mBoxOfficersFormat
     mov rsi, rbx
-    mov rdx, [officerAux + rbx]
+    mov rdx, [officerOne + rbx]
+    mov rcx, [officerTwo + rbx]
     sub rsp, 8
     call printf
     add rsp, 8
@@ -101,11 +90,17 @@ loopOfficerMovements:
     jmp loopOfficerMovements
 
 finOfficerMovementsLoop:
-    inc byte [officerIndex]
-    print saltoLinea
+    print mBoxOfficersLine
+    mov rbx, 0
+    mov bl, [mCounter]
+    cmp bl, 8
+    mov rdi, mBoxOfficersKillsFormat
+    mov rsi, rbx
+    mov rdx, [officerOne + rbx]
+    mov rcx, [officerTwo + rbx]
+    sub rsp, 8
+    call printf
+    add rsp, 8
+    print mBoxOfficersLine
+
     ret
-
-
-; 7 0 1
-; 6   2
-; 5 4 3
