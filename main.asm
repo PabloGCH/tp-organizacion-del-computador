f4ae2a38180429a36b21c9f3ecd2c3d5ac35f1a9
GianK128 <giankeberlein@gmail.com>
2024-11-12 02:34:00 -0300

    Agregar ejemplo de uso de orientacion de matriz

--- main.asm
+++ main.asm
@@ -1,13 +1,28 @@
 
 global main
+extern readRotationInput
+extern processRotation
+
 %include "macros.asm"
 
-
-section .data
-  msg db "Hello, World!", 10, 0
+section .bss
+  boardMatrix resb  144 ; Matriz de 12x12
 
 section .text
 main:
-    print msg
-    ret
-    
+  ; Pide un valor para la rotacion del tablero y la guarda en el registro al
+  call readRotationInput
+  
+  ; Procesa la rotacion y guarda el resultado (la matriz con orientacion correcta) en boardMatrix
+  ; Si la entrada no es valida, devuelve un mensaje de error en rax y guarda un cero en boardMatrix
+  call processRotation
+  cmp rax, 0
+  je invalidRotationChar
+
+  ; Continuar si la rotacion es valida
+  ; TODO: Pensar como sigue
+
+invalidRotationChar:
+  ; Imprimir mensaje de error y volver a pedir caracter
+  print rax
+  jmp main
