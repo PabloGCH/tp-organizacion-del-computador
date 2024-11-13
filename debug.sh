c1e3802f12cbd45e24c1ae5fac84721406864121
pablogch <pablochoconi@gmail.com>
2024-11-13 00:58:11 -0300

    Actualizado script de run y debug

--- debug.sh
+++ debug.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-nasm \
--i include/ \
--i macros/  \
-main.asm -f elf64 \
--g -F dwarf -l output/main.lst \
--o output/main.o
 
-gcc output/main.o -o output/main.out -no-pie
+for filename in src/*.asm; do
+  basename="${filename%.*}"
+  nasm -g -i macros/ $filename -f elf64 -o output/$basename.o
+done
+
+nasm -g -i macros/ main.asm -f elf64 -o output/main.o
+
+gcc output/src/*.o output/main.o -o output/main.out -no-pie
 
 chmod +x output/main.out
-gdb -q output/main.out
+gdb output/main.out
