d09bdcd9ff468e03c760283967dc4aea86e6cd9e
pablogch <pablochoconi@gmail.com>
2024-11-15 18:53:56 -0300

    Validacion de acercamiento a la fortaleza en movimiento de soldado

--- src/movementIsPossible.asm
+++ src/movementIsPossible.asm
@@ -2,11 +2,17 @@ global movementIsPossible
 %include "macros.asm"
 
 section .data
-  strongholdDir                       db    0           ;  0 = Up, 1 = Right, 2 = Down, 3 = Left
-  strongholdPointOfReference          db    0, 0        ;  Fila y columna fuera del tablero que sirve como punto de referencia para saber si una pieza se esta moviendo hacia la fortaleza o no.
-  canOnlyMoveTowardsStrongholdMsg     db    "Solo se puede mover hacia la fortaleza", 10, 0
-  cannotMoveMoreThanOnePositionMsg    db    "No se puede mover mas de una posicion", 10, 0
-  errorMsg                            dq    0
+  strongholdDir                         db    0           ;  0 = Up, 1 = Right, 2 = Down, 3 = Left
+  strongholdPointOfReference            dw    0, 0        ;  Fila y columna fuera del tablero que sirve como punto de referencia para saber si una pieza se esta moviendo hacia la fortaleza o no.
+  canOnlyMoveTowardsStrongholdMsg       db    "Solo se puede mover hacia la fortaleza", 10, 0
+  cannotMoveMoreThanOnePositionMsg      db    "No se puede mover mas de una posicion", 10, 0
+  errorMsg                              dq    0
+
+  distanceFromPieceToStronghold         dw    0
+  distanceFromDestinationToStronghold   dw    0
+
+  rowDistance                           dw    0
+  colDistance                           dw    0
 
 
 
@@ -17,7 +23,6 @@ section .text
   ;     RECIBE EN RDX UN NUMERO QUE INDICA TIPO DE JUGADOR 0 (Soldados) O 1 (Oficiales)
   ;     RECIBE EN RCX LA DIRECCIÓN EN LA QUE SE ENCUENTRA LA FORTALEZA (0 = Up, 1 = Right, 2 = Down, 3 = Left)
   ; POST-COND: RETORNA 0 SI EL MOVIMIENTO NO ES POSIBLE
-
 
   movementIsPossible:
     cmp     cl,    0
@@ -107,9 +112,62 @@ section .text
 
 
   ; RETORNA 0 SI NO SE ESTA ACERCANDO A LA FORTALEZA
+  ; SE USA EL METODO MANHATTAN PARA CALCULAR LA DISTANCIA ENTRE DOS PUNTOS
+
   validateGettingCloserToStronghold:
+    xor     rax,    rax
+    mov     word[distanceFromPieceToStronghold],    0
+
+    ; CALCULAR DISTANCIA ENTRE FILA DE PIEZA Y PUNTO DE REFERENCIA
+    mov     ax,    word[rsi]
+    sub     ax,    word[strongholdPointOfReference]
+
+    sub     rsp,    8
+    call    calculateModule           ; Se calcula el modulo de la diferencia
+    add     rsp,    8
+
+    add     word[distanceFromPieceToStronghold],    ax
+
+    ; CALCULAR DISTANCIA ENTRE COLUMNA DE PIEZA Y PUNTO DE REFERENCIA
+    mov     ax,    word[rsi + 2]
+    sub     ax,    word[strongholdPointOfReference + 2]
+
+    sub     rsp,    8
+    call    calculateModule           ; Se calcula el modulo de la diferencia
+    add     rsp,    8
+
+    add     word[distanceFromPieceToStronghold],    ax
+
+    ; CALCULAR DISTANCIA ENTRE FILA DE DESTINO Y PUNTO DE REFERENCIA
+    mov     ax,    word[rsi + 4]
+    sub     ax,    word[strongholdPointOfReference]
+    
+    sub     rsp,    8
+    call    calculateModule           ; Se calcula el modulo de la diferencia
+    add     rsp,    8
+
+    add     word[distanceFromDestinationToStronghold],    ax
+    
+    ; CALCULAR DISTANCIA ENTRE COLUMNA DE DESTINO Y PUNTO DE REFERENCIA
+    mov     ax,    word[rsi + 6]
+    sub     ax,    word[strongholdPointOfReference + 2]
+  
+    sub     rsp,    8
+    call    calculateModule           ; Se calcula el modulo de la diferencia
+    add     rsp,    8
+
+    add     word[distanceFromDestinationToStronghold],    ax
+    
+    mov     ax,    word[distanceFromPieceToStronghold]
+    cmp     ax,    word[distanceFromDestinationToStronghold]
+    jge     notGettingCloserToStronghold
     mov     rax,    1
     ret
+    
+    notGettingCloserToStronghold:
+      mov     qword[errorMsg],    canOnlyMoveTowardsStrongholdMsg
+      mov     rax,    0
+      ret
 
 
   initStrongholdPointOfReference:
@@ -125,23 +183,23 @@ section .text
     ret
 
     strongholdIsUp:
-      mov     byte[strongholdPointOfReference],    -1
-      mov     byte[strongholdPointOfReference + 1],    3
+      mov     word[strongholdPointOfReference],    -1
+      mov     word[strongholdPointOfReference + 1],    3
       ret
 
     strongholdIsRight:
-      mov     byte[strongholdPointOfReference],    3
-      mov     byte[strongholdPointOfReference + 1],    7
+      mov     word[strongholdPointOfReference],    3
+      mov     word[strongholdPointOfReference + 1],    7
       ret
       
     strongholdIsDown:
-      mov     byte[strongholdPointOfReference],    7
-      mov     byte[strongholdPointOfReference + 1],    3
+      mov     word[strongholdPointOfReference],    7
+      mov     word[strongholdPointOfReference + 1],    3
       ret
 
     strongholdIsLeft:
-      mov     byte[strongholdPointOfReference],    3
-      mov     byte[strongholdPointOfReference + 1],    -1
+      mov     word[strongholdPointOfReference],    3
+      mov     word[strongholdPointOfReference + 1],    -1
       ret
 
 
