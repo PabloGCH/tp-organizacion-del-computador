048584b7c9afecb017184c8bf7918a7c1cabf963
mcumskov <maxicumskov@gmail.com>
2024-11-15 00:33:11 -0300

    Now the board has indexes for cols and rows

--- src/printBoard.asm
+++ src/printBoard.asm
@@ -1,17 +1,17 @@
 ;         +---+---+---+        
-;         |   |   |   |        
+;         | X | X | X |        
 ;         +---+---+---+        
-;         |   |   |   |        
+;         | X | X | X |        
 ; +---+---+---+---+---+---+---+
-; |   |   |   |   |   |   |   |
+; | X | X | X | X | X | X | X |
 ; +---+---+---+---+---+---+---+
-; |   |   |   |   |   |   |   |
+; | X | X | X | X | X | X | X |
 ; +---+---+---+---+---+---+---+
-; |   |   |   |   |   |   |   |
+; | X | X |   |   |   | X | X |
 ; +---+---+---+---+---+---+---+
-;         |   |   |   |        
+;         |   |   | O |        
 ;         +---+---+---+        
-;         |   |   |   |        
+;         | O |   |   |        
 ;         +---+---+---+        
 
 extern printf
@@ -24,6 +24,9 @@ section .data
     bCountRows db 0
     bCountCols db 0
 
+    colsIndex db "         1   2   3   4   5   6   7    ", 10,10, 0
+    rowsIndex db "   %-1hhi   ", 0
+    rowsIndexE db "       ", 0
     bUpDown db "---", 0
     bLeftRight db "|", 0
     bCross db "+", 0
@@ -55,18 +58,25 @@ printBoard:
     mov [board], rdi
     mov [stronghold], esi
     mov [characters], edx
+
+    print newLine
+    print colsIndex
 loopRows:
     xor al, al
     mov al, [bCountRows]
     cmp al, [bRows]
     jge doneLoopRows
 
-    xor bl,bl
+    xor bl, bl
     mov cl, al
     mov bl, [bCols]
     imul rcx, rbx
-
     mov [bOffsetRows], cl
+
+    sub rsp, 8
+    call printRowIndex
+    add rsp, 8
+
     sub rsp, 8
     call loopCols
     add rsp, 8
@@ -86,7 +96,19 @@ skipIncrementRows:
 doneLoopRows:
     ret
 
+printRowIndex:
+    cmp byte [repeatFlag], 1
+    je printRowIndexEmpty
+    inc byte [bCountRows]
+    printArg rowsIndex, bCountRows
+    dec byte [bCountRows]
+    ret
+printRowIndexEmpty:
+    print rowsIndexE
+    ret
+
 printDownCrossAndLine:
+    print rowsIndexE
     not byte [repeatFlag]
     sub rsp, 8
     call loopCols
@@ -141,7 +163,7 @@ skipBoxUp:
     dec rbx
     mov r8, [board]
     mov al, [r8 + rbx]
-    mov byte [vect+rcx], al
+    mov byte [vect + rcx], al
 skipBoxPrevious:
     inc rcx
     mov al, [bCountRows]
@@ -154,7 +176,7 @@ skipBoxPrevious:
     sub bl, [bCols]
     dec bl
     mov al, [r8 + rbx]
-    mov byte [vect+rcx], al
+    mov byte [vect + rcx], al
 skipBoxPreviousUp:
     lea rdi, [vect]
     mov rsi, 4
@@ -196,14 +218,14 @@ printCross:
     cmp r8B, [stronghold]
     jl skipCrossRed
     dec r8B
-    cmp r8B, [stronghold+1]
+    cmp r8B, [stronghold + 1]
     jg skipCrossRed
 
     mov r8B, [bCountRows]
-    cmp r8B, [stronghold+2]
+    cmp r8B, [stronghold + 2]
     jl skipCrossRed
     dec r8B
-    cmp r8B, [stronghold+3]
+    cmp r8B, [stronghold + 3]
     jg skipCrossRed
 
     print cStronghold
@@ -222,20 +244,21 @@ printBoxLine:
     mov r8B, [bCountCols]
     cmp r8B, [stronghold]
     jl skipLineRed
-    cmp r8B, [stronghold+1]
+    cmp r8B, [stronghold + 1]
     jg skipLineRed
 
     mov r8B, [bCountRows]
-    cmp r8B, [stronghold+2]
+    cmp r8B, [stronghold + 2]
     jl skipLineRed
     dec r8B
-    cmp r8B, [stronghold+3]
+    cmp r8B, [stronghold + 3]
     jg skipLineRed
 
     print cStronghold
 skipLineRed:
     print bUpDown
     ret
+
 printLineEmpty:
     print cReset
     print bUpDownE
@@ -254,7 +277,7 @@ processBoxData:
     mov bl, [bTotalOffset]
     mov r8, [board]
     mov al, [r8 + rbx]
-    mov byte [vect+rcx], al
+    mov byte [vect + rcx], al
 
     mov al, [bCountCols]
     cmp al, 0
@@ -262,7 +285,7 @@ processBoxData:
     dec rbx
     inc rcx
     mov al, [r8 + rbx]
-    mov byte [vect+rcx], al
+    mov byte [vect + rcx], al
 skipBoxDataPrevious:
     lea rdi, [vect]
     mov rsi, 2
@@ -304,13 +327,13 @@ printBoxLeft:
     cmp r8B, [stronghold]
     jl skipBoxLeftRed
     dec r8B
-    cmp r8B, [stronghold+1]
+    cmp r8B, [stronghold + 1]
     jg skipBoxLeftRed
 
     mov r8B, [bCountRows]
-    cmp r8B, [stronghold+2]
+    cmp r8B, [stronghold + 2]
     jl skipBoxLeftRed
-    cmp r8B, [stronghold+3]
+    cmp r8B, [stronghold + 3]
     jg skipBoxLeftRed
 
     print cStronghold
@@ -335,6 +358,7 @@ printData:
     mov [characterAux], rax
     printArg bData, characterAux
     ret
+
 printDataEmpty:
     print cReset
     print bDataE
