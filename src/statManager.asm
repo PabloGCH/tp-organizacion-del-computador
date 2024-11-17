c99bef919dff77bdcfd5d9db16953f43ce7c803f
IGacitua <ezmagacitua@gmail.com>
2024-11-17 19:15:54 -0300

    Testeadas las subrutinas. Falta añadir una para llamar al printStats

--- src/statManager.asm
+++ src/statManager.asm
@@ -1,11 +1,11 @@
-; DIL: Posicion deseada
-; SIL: Cantidad a modificar
-; DL : Oficial a modificar. (0, 1)
+; RDI: Posicion deseada
+; RSI: Oficial a modificar. (0, 1)
+; DL : Cantidad a modificar
 ; 0-8 Primer Oficial | 9-17 Segundo oficial
 ; 0/9 Capturas. 2-8/10-17 Direcciones 
 
 section .data
-        statcounter times 18 db 0
+        statCounter times 18 db 0
 section .bss
         value resb 1
 section .text
@@ -13,20 +13,23 @@ section .text
     global statCounterGet
     ; Mueve a AL el valor del array en la posicion dil
     statCounterGet:
-        mul dl, 9
-        mov al, [statCounter + dil + dl] ; Mueve el contenido deseado a AL
+        imul rsi, 9
+        add rdi, rsi ; Posicion del array deseada
+        mov al, byte[statCounter + rdi] ; Mueve el contenido deseado a AL
         ret
 
     global statCounterAdd
     ; Le agrega al contenido del array en la posicion dil el contenido de SIL 
     statCounterAdd:
-        mul dl, 9
-        add byte[statCounter + dil + dl], sil ; Le suma al contenido de RSI (Debería ser el elemento del array) DL
+        imul rsi, 9
+        add rdi, rsi ; Posicion del array deseada
+        add byte[statCounter + rdi], dl ; Le suma al contenido de RSI (Debería ser el elemento del array) DL
         ret
 
     global statCounterSet
     ; Reemplaza al contenido del array en la posicion dil por el contenido de SIL
     statCounterSet:
-        mul dl, 9
-        mov byte[statcounter + dil + dl], sil ; Le cambia al contenido deseado al contenido de DL
+        imul rsi, 9
+        add rdi, rsi ; Posicion del array deseada
+        mov byte[statCounter + rdi], dl ; Le cambia al contenido deseado al contenido de DL
         ret
--- statCountTest.asm
+++ statCountTest.asm
@@ -4,23 +4,20 @@ global main
 extern statCounterGet
 extern statCounterSet
 
+extern statCounterAdd
+
 section .data
-    msg db "The value is: %li.", 10, 0
-    array db 1,2,3,4,5,6,7,8,9 ; Array de 9 elementos, todos contienen 1
 
 section .bss
-    result resb 1
+    result resq 1
 
 section .text
 
     main:
-        lea rsi, array
-        mov rdi, 2
-        mov dl, 116
+        mov rdi, 0
+        mov rsi, 0
+        mov dl, 5
         call statCounterSet
-        lea rsi, array
-        mov rdi, 2
+        mov rdi, 0
+        mov rsi, 0
         call statCounterGet
-        mov byte[result], al
-        printArg msg, result
-
