dc03e26c897725f0cd94e20ecfe0023b2b301309
IGacitua <ezmagacitua@gmail.com>
2024-11-19 23:50:21 -0300

    Terminado saveGame Falta loadGame

--- src/saveManager.asm
+++ src/saveManager.asm
@@ -29,13 +29,11 @@ section .bss
     savePath          resb 16; TODO validar input, si es posible
     saveFile          resq 1; "Puntero" al archivo
 
-    boardPointer      resq 1; Tablero
-    statsPointer      resq 1; Stats
-    fortressPointer   resq 1; Ubicacion fortaleza
-    fortressDirection resb 1; Direccion fortaleza
-    soldierCharacter  resb 1; Caracter Soldado
-    officerCharacter  resb 1; Caracter Oficial
-
+    boardPointer      resq 1 ; Tablero
+    statsPointer      resq 1 ; Stats
+    fortressPointer   resq 1 ; Ubicacion fortaleza
+    fortressDirection resb 1 ; Direccion fortaleza
+    characters        resb 2 ; Caracter soldado/oficial
 section .text
 
     global saveGame
@@ -47,8 +45,7 @@ section .text
         mov qword[statsPointer],     rsi
         mov qword[fortressPointer],  rdx
         mov byte[fortressDirection], r8b
-        mov byte[soldierCharacter],  cl
-        mov byte[officerCharacter],  ch
+        mov word [characters]      , cx
 
         ; read para pedir archivo a donde guardar
         read askSaveFilePath, savePath, saveFileFormat, savePath ; Es necesario validar que no sea mas de 16 caracteres?
@@ -60,11 +57,49 @@ section .text
 
         ; Guardamos la matriz
         saveBoard:
-            ; Guardamos una fila
             mov rdi, [boardPointer] ; Input
             mov rsi, 1              ; Tamaño de los elementos
-            mov rdx, 49             ; Guardamos una fila
+            mov rdx, 49             ; Cantidad de elementos
             mov rcx, [saveFile]     ; Archivo destino
+            sub rsp, 8
+            call fwrite
+            add rsp, 8
+
+        ; Guardamos el scoreboard
+        saveStats:
+            mov rdi, [statsPointer]
+            mov rsi, 1
+            mov rdx, 18
+            mov rcx, [saveFile] 
+            sub rsp, 8
+            call fwrite
+            add rsp, 8
+        
+        ; Guardamos la ubicación de la fortaleza
+        saveFortressL:
+            mov rdi, [fortressPointer]
+            mov rsi, 1
+            mov rdx, 4
+            mov rcx, [saveFile] 
+            sub rsp, 8
+            call fwrite
+            add rsp, 8
+
+        ; Guardamos la dirección de la fortaleza
+        saveFortressD:
+            mov rdi, fortressDirection ; Queremos el pointer => Sin []
+            mov rsi, 1
+            mov rdx, 1
+            mov rcx, [saveFile] 
+            sub rsp, 8
+            call fwrite
+            add rsp, 8
+
+        saveCharacters:
+            mov rdi, characters ; Queremos el pointer => Sin []
+            mov rsi, 1
+            mov rdx, 2
+            mov rcx, [saveFile]
             sub rsp, 8
             call fwrite
             add rsp, 8
