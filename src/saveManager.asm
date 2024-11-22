3e3c986d9e502ae2f2f246c82a85e1df1f58818e
IGacitua <ezmagacitua@gmail.com>
2024-11-21 23:42:46 -0300

    loadSave FUNCIONA! Falta verificar que EXISTA el archivo que se da por input

--- src/saveManager.asm
+++ src/saveManager.asm
@@ -13,7 +13,7 @@
 
 extern fopen
 extern fwrite
-extern fputs
+extern fread
 extern fclose
 
 section .data
@@ -32,12 +32,20 @@ section .bss
     saveFile          resq 1; "Puntero" al archivo
 
     ; Pointers
-    gameBoard         resq 1 ; Tablero
-    statScoreboard    resq 1 ; Stats
-    fortressLocation  resq 1 ; Ubicacion fortaleza
-    fortressDirection resq 1 ; Direccion fortaleza
-    currentTurn       resq 1 ; Turno actual
-    characters        resq 1 ; Caracter soldado/oficial
+    pGameBoard         resq 1  ; Tablero
+    pStatScoreboard    resq 1  ; Stats
+    pFortressLocation  resq 1  ; Ubicacion Fortaleza
+    pFortressDirection resq 1  ; Direccion Fortaleza
+    pCurrentTurn       resq 1  ; Turno Actual
+    pCharacters        resq 1  ; Caracter Soldado/Oficial
+
+    ; Data Storage
+    dGameBoard         times 49 resb 1 ; Tablero
+    dStatScoreboard    times 18 resb 1 ; Stats
+    dFortressLocation  times 4  resb 1 ; Ubicacion Fortaleza
+    dFortressDirection times 1  resb 1 ; Direccion Fortaleza  
+    dCurrentTurn       times 1  resb 1 ; Turno Actual
+    dCharacters        times 2  resb 1 ; Caracter Soldado/Oficial
 
 section .text
 
@@ -46,12 +54,12 @@ section .text
 
         ; Guardamos inputs en variables para no pisarlos
 
-        mov qword[gameBoard]          , rdi
-        mov qword[statScoreboard]     , rsi
-        mov qword[fortressLocation]   , rdx
-        mov qword[fortressDirection]  , rcx
-        mov qword[currentTurn]        , r8
-        mov qword[characters]         , r9
+        mov qword[pGameBoard]          , rdi
+        mov qword[pStatScoreboard]     , rsi
+        mov qword[pFortressLocation]   , rdx
+        mov qword[pFortressDirection]  , rcx
+        mov qword[pCurrentTurn]        , r8
+        mov qword[pCharacters]         , r9
 
         ; read para pedir archivo a donde guardar
         read askSaveFilePath, savePath, saveFileFormat, savePath ; Es necesario validar que no sea mas de 16 caracteres?
@@ -63,7 +71,7 @@ section .text
 
         ; Gameboard 7x7
         saveBoard:
-            mov rdi, [gameBoard]    ; Input
+            mov rdi, [pGameBoard]    ; Input
             mov rsi, 1              ; Tamaño de los elementos
             mov rdx, 49             ; Cantidad de elementos
             mov rcx, [saveFile]     ; Archivo destino
@@ -73,7 +81,7 @@ section .text
 
         ; Scoreboard 18x1
         saveStats:
-            mov rdi, [statScoreboard]
+            mov rdi, [pStatScoreboard]
             mov rsi, 1
             mov rdx, 18
             mov rcx, [saveFile] 
@@ -83,7 +91,7 @@ section .text
         
         ; Fortress Location 4x1
         saveFortressL:
-            mov rdi, [fortressLocation]
+            mov rdi, [pFortressLocation]
             mov rsi, 1
             mov rdx, 4
             mov rcx, [saveFile] 
@@ -93,7 +101,7 @@ section .text
 
         ; Fortress Direction 1x1
         saveFortressD:
-            mov rdi, [fortressDirection]
+            mov rdi, [pFortressDirection]
             mov rsi, 1
             mov rdx, 1
             mov rcx, [saveFile] 
@@ -103,7 +111,7 @@ section .text
 
         ; Current Turn 1x1
         saveTurn:
-            mov rdi, [currentTurn]
+            mov rdi, [pCurrentTurn]
             mov rsi, 1
             mov rdx, 1
             mov rcx, [saveFile]
@@ -113,7 +121,7 @@ section .text
 
         ; Game Pieces 2x1
         saveCharacters:
-            mov rdi, [characters]
+            mov rdi, [pCharacters]
             mov rsi, 1
             mov rdx, 2
             mov rcx, [saveFile]
@@ -130,4 +138,120 @@ section .text
 
     global loadGame
     loadGame:
-        ; TODO
+
+        ; Guardamos inputs en variables para no pisarlos
+
+        mov qword[pGameBoard]          , rdi
+        mov qword[pStatScoreboard]     , rsi
+        mov qword[pFortressLocation]   , rdx
+        mov qword[pFortressDirection]  , rcx
+        mov qword[pCurrentTurn]        , r8
+        mov qword[pCharacters]         , r9
+
+        ; read para pedir archivo que cargar
+        read askSaveFilePath, savePath, saveFileFormat, savePath ; Es necesario validar que no sea mas de 16 caracteres?
+        ; Abrimos el archivo en Read
+        lea rdi, savePath 
+        mov rsi, modeRead
+        call fopen
+        mov qword[saveFile], rax
+
+        load:
+            loadBoard:
+                mov rdi, dGameBoard   ; Destination
+                mov rsi, 1            ; Size
+                mov rdx, 49           ; Count
+                mov rcx, [saveFile]   ; File
+                sub rsp, 8
+                call fread
+                add rsp, 8
+
+            loadStats:
+                mov rdi, dStatScoreboard
+                mov rsi, 1
+                mov rdx, 18
+                mov rcx, [saveFile]
+                sub rsp, 8
+                call fread
+                add rsp, 8
+            
+            loadFortressL:
+                mov rdi, dFortressLocation
+                mov rsi, 1
+                mov rdx, 4
+                mov rcx, [saveFile]
+                sub rsp, 8
+                call fread
+                add rsp, 8
+
+            loadFortressD:
+                mov rdi, dFortressDirection
+                mov rsi, 1
+                mov rdx, 1
+                mov rcx, [saveFile]
+                sub rsp, 8
+                call fread
+                add rsp, 8
+
+            loadTurn:
+                mov rdi, dCurrentTurn
+                mov rsi, 1
+                mov rdx, 1
+                mov rcx, [saveFile]
+                sub rsp, 8
+                call fread
+                add rsp, 8
+
+            loadCharacters:
+                mov rdi, dCharacters
+                mov rsi, 1
+                mov rdx, 2
+                mov rcx, [saveFile]
+                sub rsp, 8
+                call fread
+                add rsp, 8
+
+        replace:
+
+            replaceBoard:
+                mov rsi,  dGameBoard  ; Source
+                mov rdi, [pGameBoard] ; Destination
+                mov rcx, 49           ; Count
+                rep movsb             ; Copies RCX bytes from [RSI] to [RDI]
+
+            replaceStats:
+                mov rsi,  dStatScoreboard
+                mov rdi, [pStatScoreboard]
+                mov rcx, 18
+                rep movsb
+
+            replaceFortressL:
+                mov rsi,  dFortressLocation
+                mov rdi, [pFortressLocation]
+                mov rcx, 4
+                rep movsb
+
+            replaceFortressD:
+                mov rsi,  dFortressDirection
+                mov rdi, [pFortressDirection]
+                mov rcx, 1
+                movsb
+
+            replaceTurn:
+                mov rsi,  dCurrentTurn
+                mov rdi, [pCurrentTurn]
+                mov rcx, 1
+                movsb
+
+            replaceCharacters:
+                mov rsi,  dCharacters
+                mov rdi, [pCharacters]
+                mov rcx, 2
+                rep movsb
+        
+        loadEnd:
+            mov rdi, [saveFile]
+            sub rsp, 8
+            call fclose
+            add rsp, 8
+            ret 
--- testMain.asm
+++ testMain.asm
@@ -1,15 +1,22 @@
 extern saveGame
+extern loadGame
 
 section .data
-    matriz      times 49 db 1
-    stats       times 18 db 2
-    fortressL   times 4  db 3
-    fortressD   times 1  db 4
-    currentTurn times 1  db 5
-    character   times 1  db "AB ", 0
+    ;matriz      times 49 db 1
+    ;stats       times 18 db 2
+    ;fortressL   times 4  db 3
+    ;fortressD   times 1  db 4
+    ;currentTurn times 1  db 5
+    ;character   times 1  db "AB ", 0
 
 section .bss
 
+    matriz      resb 49
+    stats       resb 18
+    fortressL   resb 4
+    fortressD   resb 1
+    currentTurn resb 1
+    character   resb 2
 
 section .text
 
@@ -22,6 +29,7 @@ section .text
         mov r8, currentTurn
         mov r9,  character
         sub rsp, 8
-        call saveGame
+        call loadGame
+    finalDeTodo:
         add rsp, 8
         ret
