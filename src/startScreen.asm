4946f2b9c8e0d28d56c4e5e194a2f9d6d1233223
GianK128 <giankeberlein@gmail.com>
2024-11-28 01:36:34 -0300

    Agregar tercer item a la pantalla de inicio y convertir el [0] en el Salir

--- src/startScreen.asm
+++ src/startScreen.asm
@@ -11,9 +11,10 @@ section .data
     cmd_clear         db      "clear", 0
 
     startMessageOne   db 27, '[4mEl Asalto', 27, "[0m", 10, 10, 10,0
-    startMessageTwo   db '1 - Nueva Partida', 10, 0
-    startMessageThree db '2 - Cargar Partida', 10, 0
-    startMessageFour  db '3 - Salir', 10, 0
+    startMessageTwo   db '[1] - Nueva Partida', 10, 0
+    startMessageThree db '[2] - Cargar Partida', 10, 0
+    startMessageFour  db '[3] - Opciones', 10, 0
+    startMessageFive  db '[0] - Salir', 10, 0
     startMessageInput db 10, 'Ingrese el numero de su opcion deseada: ', 0
     startMessageError db 'Input invalido, por favor ingrese un numero valido', 10, 0
 
@@ -43,6 +44,9 @@ section .text
         mov rdi, startMessageFour
         call printf
 
+        mov rdi, startMessageFive
+        call printf
+
         startScreenInput:
             mov rdi, startMessageInput
             call printf
@@ -54,7 +58,7 @@ section .text
             mov rdx, startUserChoiceNumber
             call sscanf
 
-            cmp byte[startUserChoiceNumber], 1
+            cmp byte[startUserChoiceNumber], 0
             jl startScreenError
             cmp byte[startUserChoiceNumber], 3
             jg startScreenError
